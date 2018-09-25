9395d478f236105e8ceb2e54421f9e79515565f9
John Marx <42839573+johnmarx-la@users.noreply.github.com>
2018-09-25 10:38:57 -0400

    Update deploy.sh

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -1,2 +1,7 @@
+#!/usr/bin/env bash
+
+set -x
 curl --cookie-jar cookie.txt "https://console.deployhub.com/dmadminweb/API/login?user=developer&pass=any"
+
+set -x
 curl -b cookie.txt "https://console.deployhub.com/dmadminweb/API/deploy/HelloWorldApp%3B1/GLOBAL.Linux%20Academy.DevSecOps.My%20Pipeline.Development.Dev?task=Deploy%20to%20Dev&wait=n"
